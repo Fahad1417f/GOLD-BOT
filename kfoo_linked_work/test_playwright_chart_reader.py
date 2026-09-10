680198045e48ac56a06ff5b87f44f7bbc510a8e2
Fahad Abdullah <33425724+Fahad1417f@users.noreply.github.com>
2026-09-10 10:52:08 +0300

    Add TradingView metadata parser coverage

--- kfoo_linked_work/test_playwright_chart_reader.py
+++ kfoo_linked_work/test_playwright_chart_reader.py
@@ -7,6 +7,22 @@ def test_title_parsing():
     assert tf == '15m'
 
 
+def test_xauusd_title_parsing():
+    symbol, tf = PlaywrightChartReader._parse_title('XAUUSD 4,411.735 ▲ +0.22%')
+    assert symbol == 'XAU/USD'
+    assert tf is None
+
+
+def test_visible_gold_spot_metadata():
+    symbol, tf = PlaywrightChartReader._parse_metadata(
+        'XAUUSD 4,411.735',
+        'XAUUSD\n1m\n3m\n5m\n15m\nGold Spot / U.S. Dollar\nOANDA',
+        None,
+    )
+    assert symbol == 'XAU/USD'
+    assert tf is None
+
+
 def test_no_browser_is_fail_closed():
     r = PlaywrightChartReader(cdp_url='http://127.0.0.1:1')
     result = r.connect()
@@ -16,4 +32,6 @@ def test_no_browser_is_fail_closed():
 
 if __name__ == '__main__':
     test_title_parsing()
+    test_xauusd_title_parsing()
+    test_visible_gold_spot_metadata()
     print('PLAYWRIGHT_READER_TEST=PASS')
